00be5c5103cbbd2ae79eb87065c52b59758aaeb1
dingyong87 <dingyong87@163.com>
2023-01-28 22:07:24 +0800

    1. fix get pid after start cmd. use first args.

--- dms_agent/src/script/GetPid.groovy
+++ dms_agent/src/script/GetPid.groovy
@@ -9,11 +9,18 @@ Map params = super.binding.getProperty('params') as Map
 def log = LoggerFactory.getLogger(this.getClass())
 
 String cmd = params.cmd.toString()
+// only compare start command
+def cmdArgs0 = cmd.contains(' ') ? cmd.split(' ')[0] : cmd
+
 try {
     for (pid in sigar.getProcList()) {
         def procArgs = sigar.getProcArgs(pid)
-        if (procArgs.length > 0 && cmd.startsWith(procArgs[0])) {
-            return [pid: pid]
+        if (procArgs.length > 0) {
+            def procArgs0 = procArgs[0]
+            def x = procArgs0.contains(' ') ? procArgs0.split(' ')[0] : procArgs0
+            if (cmdArgs0.startsWith(x)) {
+                return [pid: pid]
+            }
         }
     }
 
