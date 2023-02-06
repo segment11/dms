a5e9354b590db61b8009987ab1af226a84f6032e
dingyong87 <dingyong87@163.com>
2023-02-06 12:24:17 +0800

    1. support host process init shell after running.

--- dms_agent/src/script/ContainerInit.groovy
+++ dms_agent/src/script/ContainerInit.groovy
@@ -4,6 +4,10 @@ import com.github.dockerjava.api.DockerClient
 import common.Utils
 import org.slf4j.LoggerFactory
 
+import java.util.concurrent.TimeUnit
+
+import static common.ContainerHelper.isProcess
+
 DockerClient docker = super.binding.getProperty('docker') as DockerClient
 Map params = super.binding.getProperty('params') as Map
 String id = params.id
@@ -17,14 +21,24 @@ def log = LoggerFactory.getLogger('dyn')
 List<String> list = []
 for (cmdLine in initCmd.readLines().findAll { it.trim() }) {
     log.info 'ready to exec cmd - ' + cmdLine
-    String[] cmd = ['sh', '-c', cmdLine]
-    def response = docker.execCreateCmd(id).
-            withAttachStdout(true).
-            withAttachStderr(true).
-            withCmd(cmd).exec()
-    def is = docker.execStartCmd(response.id).stdin
-    String shellResult = Utils.readFully(is)
-    list << shellResult
+
+    if (isProcess(id)) {
+        def process = Runtime.getRuntime().exec(cmdLine)
+        def isOk = process.waitFor(5, TimeUnit.SECONDS)
+        log.info 'host shell execute is ok: {}', isOk
+        def is = process.inputStream
+        String shellResult = Utils.readFully(is)
+        list << shellResult
+    } else {
+        String[] cmd = ['sh', '-c', cmdLine]
+        def response = docker.execCreateCmd(id).
+                withAttachStdout(true).
+                withAttachStderr(true).
+                withCmd(cmd).exec()
+        def is = docker.execStartCmd(response.id).stdin
+        String shellResult = Utils.readFully(is)
+        list << shellResult
+    }
 }
 
 [flag: true, message: list.join("\r\n")]
