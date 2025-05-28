496fa07024dddbc0b2724e85bc188d08ed8b187b
kerry <dingyong87@163.com>
2025-05-28 19:25:12 +0800

    log: add some logs when run server.

--- dms/src/RunServer.groovy
+++ dms/src/RunServer.groovy
@@ -1,6 +1,5 @@
 import auth.User
 import com.segment.common.Conf
-import com.segment.common.Utils
 import common.Const
 import ha.JedisPoolHolder
 import io.prometheus.client.exporter.HTTPServer
@@ -73,8 +72,9 @@ if (!tableNameList.contains('CLUSTER')) {
         }
         try {
             d.exe(ddl)
+            log.info 'done execute ddl: \n {}', ddl
         } catch (Exception e) {
-            log.error 'execute ddl error, ddl: ' + ddl, e
+            log.error 'execute ddl error, ddl: {}\n', ddl, e
         }
     }
 }
@@ -117,19 +117,18 @@ cacheSupport.start()
 
 AuthTokenCacheHolder.instance.init()
 
-def localIp = Utils.localIp()
-
 // create jetty server, load route define interval using cached groovy class loader
 def server = RouteServer.instance
 server.loader = RouteRefreshLoader.create(loader.gcl).addClasspath(srcDirPath).addClasspath(resourceDirPath).
         addDir(c.projectPath('/src/ctrl')).jarLoad(c.isOn('server.runtime.jar'))
 server.webRoot = c.projectPath('/www')
-server.start(Const.SERVER_HTTP_LISTEN_PORT, localIp)
+server.start(Const.SERVER_HTTP_LISTEN_PORT, '0.0.0.0')
+log.info 'server started - http://localhost:{}/admin/', Const.SERVER_HTTP_LISTEN_PORT
 
 // prometheus metrics
 DefaultExports.initialize()
-def metricsServer = new HTTPServer(localIp, Const.METRICS_HTTP_LISTEN_PORT, true)
-log.info 'metrics server started - {}', Const.METRICS_HTTP_LISTEN_PORT
+def metricsServer = new HTTPServer('0.0.0.0', Const.METRICS_HTTP_LISTEN_PORT, true)
+log.info 'metrics server started - http://localhost:{}', Const.METRICS_HTTP_LISTEN_PORT
 
 def stopCl = {
     metricsServer.close()
